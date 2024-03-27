03c05b1ecae5d375bbc9805bd69affbea151ba62
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-03-26 21:30:03 -0300

    ip api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // apiUrl: 'http://159.203.96.72:8080'
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://10.0.0.253:8080'
+  // apiUrl: 'http://localhost:8080'
 }
