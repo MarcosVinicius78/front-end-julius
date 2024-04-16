4d2ff4a1f69c7d67a9404cd50bf166055e8a5d27
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-04-15 22:16:59 -0300

    https para a api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // apiUrl: 'http://10.0.0.12:8080'
   // apiUrl: 'http://localhost:8080'
-  apiUrl: 'http://systemdevmv.site/api'
+  apiUrl: 'https://systemdevmv.site/api'
 }
