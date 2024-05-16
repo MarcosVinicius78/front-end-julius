a16a9839d9a450a0290ea2bdfd7aafca876bef22
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-05-16 15:26:15 -0300

    https na api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   // apiUrl: 'http://10.0.0.12:8080'
   // apiUrl: 'http://localhost:8080'
   // apiUrl: 'http://:8080'
-  apiUrl: 'http://www.systemdevmv.site/api'
+  apiUrl: 'https://www.systemdevmv.site/api'
   // apiUrl: 'https://fc6mxmb4-8080.brs.devtunnels.ms'
 }
