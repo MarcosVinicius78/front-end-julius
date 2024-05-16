6896bae9e815a4deb4fcd31dbb8f67f6c30324a1
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-05-16 17:58:11 -0300

    teste

---
+++ src/main.server.ts
@@ -0,0 +1,9 @@
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+import { AppServerModule } from './app/AppServerModule';
+
+if (true) {
+  enableProdMode();
+}
+
+export { AppServerModule };
