f6d2954737d1a37d763737c0df272ee8552b1b58
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-05-21 23:09:56 -0300

    dominio sergipe ofertas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   // apiUrl: 'http://localhost:8080'
   // apiUrl: 'http://:8080'
   // apiUrl: 'https://www.systemdevmv.site/api'
-  apiUrl: 'http://www.systemdevmv.site/api'
+  // apiUrl: 'http://www.systemdevmv.site/api'
+  apiUrl: 'http://www.sergipeofertas.com.br/api'
   // apiUrl: 'https://fc6mxmb4-8080.brs.devtunnels.ms'
 }
