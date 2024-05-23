f01797c7ff09f9bb1e552cf12f403fe9a6b16810
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-05-23 18:14:01 -0300

    teste scroll position rotation enable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -51,7 +51,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabledBlocking',
-    scrollPositionRestoration: 'top'
+    scrollPositionRestoration: 'enabled'
   })],
   exports: [RouterModule]
 })
