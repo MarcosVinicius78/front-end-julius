245ede0d495d22a867638af9498289cfcf8fd28f
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-08-01 22:27:16 -0300

    pequenas alterações

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LinksBanner } from 'src/app/dto/LinksBanner';
 import { PostDTO, PostDTOGeral, PostsLista } from 'src/app/dto/postDTO';
+import { LinkBannerService } from 'src/app/service/painel/link-banner.service';
 import { PostService } from 'src/app/service/painel/post.service';
 import { environment } from 'src/environments/environment';
 
@@ -14,6 +16,7 @@ export class BlogComponent {
   posts!: PostsLista[];
 
   post!: PostDTOGeral;
+  links!: LinksBanner;
 
   apiUrl: string = environment.apiUrl
 
@@ -21,15 +24,17 @@ export class BlogComponent {
 
   constructor(
     private postService: PostService,
-    private router: ActivatedRoute
+    private router: ActivatedRoute,
+    private linkBannerService: LinkBannerService
   ){}
 
   ngOnInit(): void {
     this.id = this.router.snapshot.paramMap.get('id')!;
+    this.pegarLinks()
     if (this.id === null) {
       this.listarPosts();
     }else{
-      this.pegarPost()
+      this.pegarPost();
     }
   }
 
@@ -55,5 +60,11 @@ export class BlogComponent {
 
   getTextWithoutTags(html: string): string {
     return html.replace(/<[^>]*>/g, '');
+  }
+
+  pegarLinks(){
+    this.linkBannerService.listarLinksEBanners().subscribe(response => {
+      this.links = response;
+    });
   }
 }
