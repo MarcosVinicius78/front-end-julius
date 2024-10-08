fddc4897dd0fa9293d2110d60e84509e1f9e26e8
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-10-07 22:39:51 -0300

    Alteracao no menu mobile e na categoria drop

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,15 +4,28 @@ import { Categoria } from 'src/app/models/categoria';
 import { isPlatformBrowser } from '@angular/common';
 import { LinkBannerService } from 'src/app/service/painel/link-banner.service';
 import { LinksBanner } from 'src/app/dto/LinksBanner';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('categoria', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10%)' }),
+        animate('300ms ease', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease', style({ opacity: 0, transform: 'translateY(0%)' }))
+      ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
 
   menuValue:boolean=false;
+  abrirCategoria:boolean=false;
   menu_icon :string ='pi pi-bars';
   links = new LinksBanner();
 
@@ -33,10 +46,12 @@ export class HeaderComponent implements OnInit {
   }
 
   toggleCategoria(): void {
-      const dropdown = document.getElementById('categ');
-      if (dropdown) {
-        dropdown.classList.toggle('show');
-      }
+      // const dropdown = document.getElementById('categ');
+      // if (dropdown) {
+      //   dropdown.classList.toggle('show');
+      // }
+    this.abrirCategoria =! this.abrirCategoria
+
   }
 
   openMenu(){
