8c6cf9461d56fa0eadfc31e954d583393c1586a4
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-11-30 23:21:11 -0300

    alteração

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   // apiUrl: 'http://62.72.11.56/api'
-  // apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080',
 
   site: 1,
   // apiUrl: 'https://www.systemdevmv.site/api'
   // apiUrl: 'http://www.systemdevmv.site/api'
 
   // esse aqui
-  apiUrl: 'https://sergipeofertas.com.br/api'
+  // apiUrl: 'https://sergipeofertas.com.br/api'
 
   // apiUrl: 'http://www.sergipeofertas.com.br/api'
   // apiUrl: 'https://fc6mxmb4-8080.brs.devtunnels.ms'
